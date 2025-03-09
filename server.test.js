2ec91cb2f155a829733ca987b1f32715e0afce61
praveen felix <praveenfelix.career@gmail.com>
2025-03-09 00:55:57 -0500

    updated server.test.js

--- server.test.js
+++ server.test.js
@@ -21,7 +21,7 @@ describe('Weather API Endpoints', () => {
     });
 });*/
 
-const request = require('supertest');
+/*const request = require('supertest');
 const app = require('./server'); // Import your Express app
 
 describe('Weather API Endpoints', () => {
@@ -35,6 +35,39 @@ describe('Weather API Endpoints', () => {
     // Close the server after tests
     afterAll((done) => {
         server.close(done); // Close the server and call done() to signal Jest
+    });
+
+    it('GET /api/weather without city parameter should return 400', async () => {
+        const res = await request(app).get('/api/weather');
+        expect(res.statusCode).toEqual(400);
+    });
+
+    it('GET /api/weather with invalid city should return 500', async () => {
+        const res = await request(app).get('/api/weather?city=InvalidCity');
+        expect(res.statusCode).toEqual(500);
+    });
+});*/
+
+const request = require('supertest');
+const app = require('./server'); // Import your Express app
+
+describe('Weather API Endpoints', () => {
+    let server;
+
+    // Start the server before tests
+    beforeAll((done) => {
+        server = app.listen(3000, () => {
+            console.log('Test server started on port 3000');
+            done(); // Signal Jest that the server has started
+        });
+    });
+
+    // Close the server after tests
+    afterAll((done) => {
+        server.close(() => {
+            console.log('Test server closed');
+            done(); // Signal Jest that the server has closed
+        });
     });
 
     it('GET /api/weather without city parameter should return 400', async () => {
